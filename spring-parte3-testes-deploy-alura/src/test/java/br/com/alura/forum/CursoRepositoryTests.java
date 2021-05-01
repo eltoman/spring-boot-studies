16549be33080ed3411fd849dc1eee31d5d5d8b80
Elton Manoel <eltonmds@usp.br>
2021-05-01 19:50:59 -0300

    Add repository unit test using AutoConfigureTestDatabase NONE

--- spring-parte3-testes-deploy-alura/src/test/java/br/com/alura/forum/CursoRepositoryTests.java
+++ spring-parte3-testes-deploy-alura/src/test/java/br/com/alura/forum/CursoRepositoryTests.java
@@ -6,18 +6,28 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTests {
 
 	@Autowired
 	private CursoRepository cursoRepository;
 
+	@Autowired
+	private TestEntityManager testEntityManager;
+
 	@Test
 	public void deveriaBuscarCursoPorNome() {
+		createCurso();
 		String nomeCurso = "HTML 5";
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 		Assert.assertNotNull(curso);
@@ -29,5 +39,11 @@ public class CursoRepositoryTests {
 		String nomeCurso = "Curso que nao existe";
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 		Assert.assertNull(curso);
+	}
+
+	public void createCurso(){
+		Curso curso = new Curso();
+		curso.setNome("HTML 5");
+		testEntityManager.persist(curso);
 	}
 }
